a3f4065d847857258a37d34830f6cb7c0ffb6052
Muhammad Ali Khan <muhammadalikhanhitec@gmail.com>
2026-06-30 12:27:56 +0200

    Add: pow-x-n - submission-2

---
+++ Data Structures & Algorithms/pow-x-n/submission-2.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double result = 1;
+        for(int i = 0; i < abs(n); i++){
+            result = result * x;
+        }
+        if(n <  0){
+            return 1/result;
+        }
+        return result;
+    }
+};

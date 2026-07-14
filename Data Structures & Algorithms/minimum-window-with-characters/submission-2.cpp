1af1231f57801c55a22e1f0693a196fd7d558e6b
Khan Muhammad Ali <SD22I2@tr.pte.hu>
2026-07-14 17:00:24 +0200

    Add: minimum-window-with-characters - submission-2

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-2.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        if(t.empty()){
+            return " ";
+        }
+        unordered_map<char,int>countT,window;
+        for(char c : t){
+            countT[c]++;
+        }
+        int stock = 0;
+        int n = countT.size();
+        std::pair<int,int> res = {-1,-1};
+        int reslen = INT_MAX;
+        int left = 0;
+        for(int right = 0; right < s.size(); right++){
+            char c = s[right];
+            window[c]++;
+            if(countT.count(c) && window[c] == countT[c]){
+                stock++;
+            }
+            while(stock == n){
+                if((right - left + 1) < reslen){
+                    reslen = right - left + 1;
+                    res = {left , right};
+                }
+                window[s[left]]--;
+                if(countT.count(s[left]) && window[s[left]] < countT[s[left]]){
+                    stock--;
+                }
+                left++;
+            }
+        }
+    if(reslen == INT_MAX){
+        return "";
+    }
+    else{
+        return s.substr(res.first,reslen);
+    }
+    }
+};
